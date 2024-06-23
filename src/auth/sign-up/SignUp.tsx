6e176958592267e3691e3c350f05725b9f9c2021
Lenox <lenoxmiheso@gmail.com>
2024-06-23 19:50:16 +0300

    Solving issues

--- src/auth/sign-up/SignUp.tsx
+++ src/auth/sign-up/SignUp.tsx
@@ -7,7 +7,7 @@ import {useNavigate} from 'react-router-dom';
 
 
 const SignUp = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleLoginRedirect = () => {
     navigate('/login');
